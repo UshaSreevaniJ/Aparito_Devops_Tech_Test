924b06e16776c865aec421b0781202d0e0c90ec9
RahulThammu <rahul.thammu@gmail.com>
2023-04-23 11:36:09 +0100

    Changes to variables file

--- variables.tf
+++ variables.tf
@@ -6,3 +6,18 @@ variable "keypair" {
   description = "Adding the SSH authorized key"
   type        = string
 }
+
+variable "cidr_block" {
+  description = "cidr for the vpc"
+  type        = string
+}
+
+variable "public_subnet_range" {
+  description = "IP Range of this subnet"
+  type        = list(string)
+}
+
+variable "az_public" {
+  description = "AZ of this subnet"
+  type        = list(string)
+}
